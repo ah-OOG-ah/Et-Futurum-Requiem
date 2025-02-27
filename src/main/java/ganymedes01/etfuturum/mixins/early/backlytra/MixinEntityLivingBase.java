3a423f0978fb06f1e2631db0e3e9389408a0c18f
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-02-27 01:20:41 -0500

    Clean up constants and floats

--- src/main/java/ganymedes01/etfuturum/mixins/early/backlytra/MixinEntityLivingBase.java
+++ src/main/java/ganymedes01/etfuturum/mixins/early/backlytra/MixinEntityLivingBase.java
@@ -29,51 +29,51 @@ public abstract class MixinEntityLivingBase extends Entity {
 		/* method is named incorrectly in these older mappings, it's really isServerWorld */
 		if (this.isClientWorld() && !this.isInWater() && !this.handleLavaMovement()) {
 			if (this instanceof IElytraPlayer && ((IElytraPlayer) this).etfu$isElytraFlying()) {
-				if (this.motionY > -0.5D) {
-					this.fallDistance = 1.0F;
+				if (this.motionY > -0.5) {
+					this.fallDistance = 1;
 				}
 
 				Vec3 vec3d = this.getLookVec();
-				float f = this.rotationPitch * 0.017453292F;
+				double f = Math.toRadians(this.rotationPitch);
 				double d6 = Math.sqrt(vec3d.xCoord * vec3d.xCoord + vec3d.zCoord * vec3d.zCoord);
 				double d8 = Math.sqrt(this.motionX * this.motionX + this.motionZ * this.motionZ);
 				double d1 = vec3d.lengthVector();
-				float f4 = MathHelper.cos(f);
-				f4 = (float) ((double) f4 * (double) f4 * Math.min(1.0D, d1 / 0.4D));
-				this.motionY += -0.08D + f4 * 0.06D;
+				double f4 = Math.cos(f);
+				f4 = f4 * f4 * Math.min(1, d1 / 0.4);
+				this.motionY += -0.08 + f4 * 0.06;
 
-				if (this.motionY < 0.0D && d6 > 0.0D) {
-					double d2 = this.motionY * -0.1D * f4;
+				if (this.motionY < 0 && d6 > 0) {
+					double d2 = this.motionY * -0.1 * f4;
 					this.motionY += d2;
 					this.motionX += vec3d.xCoord * d2 / d6;
 					this.motionZ += vec3d.zCoord * d2 / d6;
 				}
 
-				if (f < 0.0F) {
-					double d9 = d8 * (-MathHelper.sin(f)) * 0.04D;
-					this.motionY += d9 * 3.2D;
+				if (f < 0) {
+					double d9 = d8 * (-Math.sin(f)) * 0.04;
+					this.motionY += d9 * 3.2;
 					this.motionX -= vec3d.xCoord * d9 / d6;
 					this.motionZ -= vec3d.zCoord * d9 / d6;
 				}
 
-				if (d6 > 0.0D) {
-					this.motionX += (vec3d.xCoord / d6 * d8 - this.motionX) * 0.1D;
-					this.motionZ += (vec3d.zCoord / d6 * d8 - this.motionZ) * 0.1D;
+				if (d6 > 0) {
+					this.motionX += (vec3d.xCoord / d6 * d8 - this.motionX) * 0.1;
+					this.motionZ += (vec3d.zCoord / d6 * d8 - this.motionZ) * 0.1;
 				}
 
-				this.motionX *= 0.9900000095367432D;
-				this.motionY *= 0.9800000190734863D;
-				this.motionZ *= 0.9900000095367432D;
+				this.motionX *= 0.99;
+				this.motionY *= 0.98;
+				this.motionZ *= 0.99;
 				this.moveEntity(this.motionX, this.motionY, this.motionZ);
 
 				if (this.isCollidedHorizontally && !this.worldObj.isRemote) {
 					double d10 = Math.sqrt(this.motionX * this.motionX + this.motionZ * this.motionZ);
 					double d3 = d8 - d10;
-					float f5 = (float) (d3 * 10.0D - 3.0D);
+					double f5 = d3 * 10 - 3;
 
-					if (f5 > 0.0F) {
-						this.playSound((int) f5 > 4 ? "game.player.hurt.fall.big" : "game.player.hurt.fall.small", 1.0F, 1.0F);
-						this.attackEntityFrom(flyIntoWall, f5);
+					if (f5 > 0) {
+						this.playSound((int) f5 > 4 ? "game.player.hurt.fall.big" : "game.player.hurt.fall.small", 1, 1);
+						this.attackEntityFrom(flyIntoWall, (float) f5);
 					}
 				}
 
