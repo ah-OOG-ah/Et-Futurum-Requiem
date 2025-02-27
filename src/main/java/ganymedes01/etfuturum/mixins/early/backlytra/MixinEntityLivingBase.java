12517d5c97c851dd656c688a662637f675ed2c07
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-02-27 01:38:59 -0500

    Enlighten variables

--- src/main/java/ganymedes01/etfuturum/mixins/early/backlytra/MixinEntityLivingBase.java
+++ src/main/java/ganymedes01/etfuturum/mixins/early/backlytra/MixinEntityLivingBase.java
@@ -1,10 +1,14 @@
 package ganymedes01.etfuturum.mixins.early.backlytra;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.cos;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.toRadians;
+
 import ganymedes01.etfuturum.elytra.IElytraPlayer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.MathHelper;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -33,47 +37,51 @@ public abstract class MixinEntityLivingBase extends Entity {
 					this.fallDistance = 1;
 				}
 
-				Vec3 vec3d = this.getLookVec();
-				double f = Math.toRadians(this.rotationPitch);
-				double d6 = Math.sqrt(vec3d.xCoord * vec3d.xCoord + vec3d.zCoord * vec3d.zCoord);
-				double d8 = Math.sqrt(this.motionX * this.motionX + this.motionZ * this.motionZ);
-				double d1 = vec3d.lengthVector();
-				double f4 = Math.cos(f);
-				f4 = f4 * f4 * Math.min(1, d1 / 0.4);
-				this.motionY += -0.08 + f4 * 0.06;
+				Vec3 lookVec = this.getLookVec();
+				double pitchRadians = toRadians(this.rotationPitch);
 
-				if (this.motionY < 0 && d6 > 0) {
-					double d2 = this.motionY * -0.1 * f4;
+				double horizontalPos = sqrt(lookVec.xCoord * lookVec.xCoord + lookVec.zCoord * lookVec.zCoord);
+				double horizontalSpeed = sqrt(this.motionX * this.motionX + this.motionZ * this.motionZ);
+
+                double cos2Pitch = cos(pitchRadians);
+				cos2Pitch = cos2Pitch * cos2Pitch;
+				this.motionY += -0.08 + cos2Pitch * 0.06;
+
+				if (this.motionY < 0 && horizontalPos > 0) {
+					double d2 = this.motionY * -0.1 * cos2Pitch;
 					this.motionY += d2;
-					this.motionX += vec3d.xCoord * d2 / d6;
-					this.motionZ += vec3d.zCoord * d2 / d6;
+					this.motionX += lookVec.xCoord * d2 / horizontalPos;
+					this.motionZ += lookVec.zCoord * d2 / horizontalPos;
 				}
 
-				if (f < 0) {
-					double d9 = d8 * (-Math.sin(f)) * 0.04;
+				if (pitchRadians < 0) {
+					double d9 = horizontalSpeed * (-Math.sin(pitchRadians)) * 0.04;
 					this.motionY += d9 * 3.2;
-					this.motionX -= vec3d.xCoord * d9 / d6;
-					this.motionZ -= vec3d.zCoord * d9 / d6;
+					this.motionX -= lookVec.xCoord * d9 / horizontalPos;
+					this.motionZ -= lookVec.zCoord * d9 / horizontalPos;
 				}
 
-				if (d6 > 0) {
-					this.motionX += (vec3d.xCoord / d6 * d8 - this.motionX) * 0.1;
-					this.motionZ += (vec3d.zCoord / d6 * d8 - this.motionZ) * 0.1;
+				if (horizontalPos > 0) {
+					this.motionX += (lookVec.xCoord / horizontalPos * horizontalSpeed - this.motionX) * 0.1;
+					this.motionZ += (lookVec.zCoord / horizontalPos * horizontalSpeed - this.motionZ) * 0.1;
 				}
 
 				this.motionX *= 0.99;
 				this.motionY *= 0.98;
 				this.motionZ *= 0.99;
+
+				// DaBR hooks here! //
+
 				this.moveEntity(this.motionX, this.motionY, this.motionZ);
 
 				if (this.isCollidedHorizontally && !this.worldObj.isRemote) {
-					double d10 = Math.sqrt(this.motionX * this.motionX + this.motionZ * this.motionZ);
-					double d3 = d8 - d10;
-					double f5 = d3 * 10 - 3;
+					double newHorizontalSpeed = sqrt(this.motionX * this.motionX + this.motionZ * this.motionZ);
+					double speedChange = horizontalSpeed - newHorizontalSpeed;
+					double damage = speedChange * 10 - 3;
 
-					if (f5 > 0) {
-						this.playSound((int) f5 > 4 ? "game.player.hurt.fall.big" : "game.player.hurt.fall.small", 1, 1);
-						this.attackEntityFrom(flyIntoWall, (float) f5);
+					if (damage > 0) {
+						this.playSound((int) damage > 4 ? "game.player.hurt.fall.big" : "game.player.hurt.fall.small", 1, 1);
+						this.attackEntityFrom(flyIntoWall, (float) damage);
 					}
 				}
 
